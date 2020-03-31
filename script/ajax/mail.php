3f315286c709aa10f69f717d567cfdeaba63eea3
CodeBuilderPRO <CodeBuilderPRO@users.noreply.github.com>
2020-03-31 15:16:04 +0300

    CodePRO
    Code

---
+++ script/ajax/mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "annaberd1@gmail.com";
+$sitename = "hannahrytsyk.com";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон: $phone \nЭлектронная почта: $email \nОбращение: $text";
+// $message = "Имя: $name \nТелефон: $phone";
+
+$pagetitle = "Заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
---
+++ script/script.js
@@ -0,0 +1,14 @@
+window.onload= function() {
+  Particles.init({
+    selector: '.background',
+    // Colors: #000000,
+    maxParticles: 30,
+    connectParticles: true,
+    sizeVariations: 0,
+    
+  });
+};
+
+$(function(){
+    $('.circlestat').circliful();
+ });
